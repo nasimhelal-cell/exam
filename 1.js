a3b29de8dc32944c032ee59db3eb265ab5b4cc20
Nasim <nasimhelal123>
2023-01-24 21:36:31 +0600

    first problem is solved

--- 1.js
+++ 1.js
@@ -1,1 +1,32 @@
-//
+let firstArray = [1, 4, 5, 7, 8, 10];
+let secondArray = [2, 3, 5, 7, 14, 18];
+
+function coherrence(firstArray, secondArray) {
+  let coherrenceArray = [];
+
+  for (let i = 0; i < firstArray.length; i++) {
+    if (i >= 2 && i <= 3) i += 2;
+    coherrenceArray.push(firstArray[i]);
+  }
+  for (let i = 0; i < secondArray.length; i++) {
+    if (i >= 2 && i <= 3) i += 2;
+    coherrenceArray.push(secondArray[i]);
+  }
+  return coherrenceArray;
+}
+console.log(coherrence(firstArray, secondArray));
+
+//bubble sort
+
+function bubbleSort(a) {
+  for (i = 0; i < a.length; i++) {
+    for (j = 0; j < a.length - 1 - i; j++) {
+      if (a[j] > a[j + 1]) {
+        [a[j + 1], a[j]] = [a[j], a[j + 1]];
+      }
+    }
+  }
+  return a;
+}
+
+console.log(bubbleSort(coherrence(firstArray, secondArray)));
